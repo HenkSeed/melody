6e4e6d56d3a5fd90f1cf8f2cb402b941227f098e
HenkSeed <55748514+HenkSeed@users.noreply.github.com>
2021-09-02 00:46:33 +0700

    Добавлена синхронность выделения квартир на плане и в списке

--- js/main.js
+++ js/main.js
@@ -2,17 +2,71 @@
 $(document).ready(function () {
 	// При наведении мышки на этаж, его номер появляется на счётчике
 	let currentFloor = 2; // Переменная, где хранится значение текущего этажа
-	let floorPath = $('.home-image path'); // Каждый отдельный этаж в SVG
+	let floorPath = $('.home-image path'); // Массив слоёв выделения этажей в файле SVG
 	let counterUp = $('.counter-up'); // Кнопка увеличения номера этажа
 	let counterDown = $('.counter-down'); // Кнопка уменьшения номера этажа
-	let modal = $('.modal');
-	let modalCloseButton = $('.modal-close-button');
-	let viewFlatsButton = $('.view-flats');
+	let modal = $('.modal'); // Модальное окно
+	let modalCloseButton = $('.modal-close-button'); // Кнопка закрытия модального окна
+	let viewFlatsButton = $('.view-flats'); // Кнопка "Смотреть квартиры"
+	let dataFlats = $('.flats path'); // Все квартиры на плане \ JQuery позволяет обращаться
+	let flatLink = $('.flat-link'); // Все квартиры в списке   / сразу ко всем элементам
+	let currentFlat; // Сохраняет индекс выбранной на плане квартиры
+	let currentLink; // Сохраняет индекс выбранной в списке квартиры
+	const flatsLeave = document.querySelectorAll('.flats path'); // Все квартиры на плане
+	const listLeave = document.querySelectorAll('.flat-link'); // Все квартиры в списке
 
-	// Функция открытия/закрытия модального окна через константу
-	// const toggleModal = function () {
-	// 	modal.toggleClass('is-open');
-	// };
+	// =====================================================================
+	// Убирает выделение квартир в списке, когда указатель мыши покидает план этажа
+	flatsLeave.forEach((elem) => {
+		elem.addEventListener('mouseleave', () => {
+			flatLink.removeClass('flat-link-hover');
+		});
+	});
+	// ---------------------------------------------------------------------
+
+	// =====================================================================
+	// Убирает выделение квартир на плане, когда указатель мыши покидает список
+	listLeave.forEach((elem) => {
+		elem.addEventListener('mouseleave', () => {
+			dataFlats.removeClass('flats-hover');
+		});
+	});
+	// ---------------------------------------------------------------------
+
+	// =====================================================================
+	// При наведении на плане этажа указателя мыши на квартиру
+	// эта же квартира подсвечивается в списке
+	dataFlats.on('mouseover', function () {
+		// Удаляется класс текущей квартиры (убирается накопление подсветки позиций в списке)
+		// При переходе указателя мыши на плане этажа с одной квартиры на другую
+		flatLink.removeClass('flat-link-hover');
+		// Убирается подсветка на плане этажа, вызванная наведением указателя в списке
+		// (когда происходит возврат указателя мыши из списка на план этажа)
+		dataFlats.removeClass('flats-hover');
+		// Получается номер квартиры и присваивается переменной currentFlat
+		currentFlat = $(this).attr('data-flats');
+		// Выделяет в списке квартиру, подсвеченную на плане этажа
+		$(`[data-link=${currentFlat}]`).toggleClass('flat-link-hover');
+	});
+	// ---------------------------------------------------------------------
+
+	// =====================================================================
+	// При наведение указателя мыши на квартиру в списке квартир этажа
+	// эта же квартира подсвечивается на плане этажа
+	flatLink.on('mouseover', function () {
+		// Удаляется класс текущей квартиры (убирается накопление подсветки квартир
+		// на плане этажа) при переходе указателя мыши по списку квартир
+		dataFlats.removeClass('flats-hover');
+		// Убирается подсветка квартиры в списке, вызванная наведением указателя мыши
+		// на плане этажа (когда происходит возврат указателя мыши с плана этажа
+		// на список)
+		flatLink.removeClass('flat-link-hover');
+		// Получается номер квартиры и присваивается переменной currentLink
+		currentLink = $(this).attr('data-link');
+		// Выделяет на плане этажа квартиру, подсвеченную  в списке
+		$(`[data-flats=${currentLink}]`).toggleClass('flats-hover');
+	});
+	// ---------------------------------------------------------------------
 
 	// Открытие модального окна при нажатии на этаж
 	floorPath.on('click', toggleModal);
@@ -40,7 +94,7 @@ $(document).ready(function () {
 				useGroupping: false,
 			});
 			$('.counter').text(usCurrentFloor); // Записываем номер этажа в счётчик
-			floorPath.removeClass('current-floor');
+			floorPath.removeClass('current-floor'); // Убираем подсветку =старого= этажа
 			// Подсвечиваем текущий этаж при нажатии кнопки "ВВЕРХ"
 			$(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
 		}
@@ -56,7 +110,7 @@ $(document).ready(function () {
 				useGroupping: false,
 			});
 			$('.counter').text(usCurrentFloor);
-			floorPath.removeClass('current-floor');
+			floorPath.removeClass('current-floor'); // Убираем подсветку =старого= этажа
 			// Подсвечиваем текущий этаж при нажатии кнопки "ВНИЗ"
 			$(`[data-floor=${usCurrentFloor}]`).toggleClass('current-floor');
 		}
